fc693ff48ed07d4c9a3f025f1906e533effcd9db
Dominik Kreiselmaier <D.Kreiselmaier.inf21@stud.bbbaden.ch>
2023-11-30 09:08:29 +0100

    add basic interfaces
    Co-authored-by: sanqro <contact@sanqro.xyz>

---
+++ server/src/interfaces/interfaces.ts
@@ -0,0 +1,14 @@
+export interface ILoginForm {
+  username: string;
+  password: string;
+}
+
+export interface IRegisterForm {
+  username: string;
+  password: string;
+  email: string;
+}
+
+export interface IJWTData {
+  username: string;
+}
