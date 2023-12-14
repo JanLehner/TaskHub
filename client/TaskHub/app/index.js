c56a46c38eb51bd0fde958cfe677fc2ae47adfd6
Jan Lehner <janlehner.05@gmail.com>
2023-12-14 08:19:03 +0100

    Implemented a new scheme for routing

---
+++ client/TaskHub/app/index.js
@@ -0,0 +1,9 @@
+import { View, Text, Pressable } from "react-native";
+import React, { useState, useEffect } from "react";
+import { styles } from "./stylesheets/style";
+
+export default function LocationScreen() {
+    return <View style={styles.container}>
+      Test
+  </View>;
+};
---
+++ client/TaskHub/app/stylesheets/style.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+const lightPurple = "#9D8FC5";
+const darkPurple = "#6759A7";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  flexbox: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
