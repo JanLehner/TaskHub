163ed8a5da586ceca168eb87545bb1bdfb9475cc
sanqro <contact@sanqro.xyz>
2023-12-20 20:47:18 +0100

    add endpoint to delete task
    Co-authored-by: Niels Brunokowski <77078828+Neox420@users.noreply.github.com>
    Co-authored-by: RelxOff <doeme.kreisel@gmail.com>

--- server/src/routes/task.ts
+++ server/src/routes/task.ts
@@ -47,4 +47,52 @@ router.post("/create/:board", async (req, res) => {
 });
 
 
+router.post("/delete/:board/", async (req, res) => {
+  try {
+    const taskDataSet: ITask = req.body as ITask;
+    const key = taskDataSet.title.trim() + taskDataSet.owner.trim();
+    const boardTitle = req.params.board;
+    if (!taskDataSet.title || !taskDataSet.owner ) {
+      throw new Error("Both 'title' and 'owner' are required fields.");
+    }
+
+    const taskDataSetJSON = {
+      title: taskDataSet.title,
+      description: taskDataSet.description,
+      dueDate: taskDataSet.dueDate,
+      owner: taskDataSet.owner
+    };
+
+    const existing =  await boardSets.get(boardTitle);
+    const currentTasks = existing.tasks;
+    let taskFound = false;
+    existing.tasks = (currentTasks as any[]).filter(task => {
+      const isMatch = task.title === taskDataSetJSON.title 
+      && task.owner === taskDataSetJSON.owner 
+      && task.description === taskDataSetJSON.description
+      && task.dueDate === taskDataSetJSON.dueDate;
+      if (isMatch) {
+        taskFound = true;
+      }
+      return !isMatch;
+    });
+
+    if (!taskFound) {
+      throw new Error("Task not found.");
+    }
+
+    await boardSets.delete(boardTitle);
+    const newtaskDataSet = await boardSets.insert(existing);
+    res.status(201).json({
+      title: taskDataSet.title,
+      description: taskDataSet.description,
+      dueDate: taskDataSet.dueDate,
+      owner: taskDataSet.owner,
+      success: true
+    });
+  } catch (err) {
+    res.status(503).json({ error: err.message });
+  }
+});
+
 export default router;
