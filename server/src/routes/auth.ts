adb335758708ee64f3645ad78c334c285e9f8072
sanqro <contact@sanqro.xyz>
2023-12-20 18:15:10 +0100

    add login endpoint
    Co-authored-by: Niels Brunokowski <77078828+Neox420@users.noreply.github.com>
    Co-authored-by: RelxOff <doeme.kreisel@gmail.com>

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -3,7 +3,8 @@ import * as dotenv from "dotenv";
 import path from "path";
 import { Deta } from "deta";
 import argon2 from "argon2";
-import { IRegisterForm } from "../interfaces/interfaces";
+import { ILoginForm, IRegisterForm } from "../interfaces/interfaces";
+import jwt from "jsonwebtoken";
 dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 // deta setup
@@ -37,6 +38,35 @@ router.post("/register", async (req, res) => {
       });
     } else {
       throw new Error("Failed to register user!");
+    }
+  } catch (err) {
+    res.status(503).json({ error: err.message });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const loginFormData: ILoginForm = req.body as ILoginForm;
+
+    if (loginFormData.username == null || loginFormData.password == null || loginFormData.username == "" || loginFormData.password == "") {
+      throw new Error("Invalid Request");
+    }
+
+    const user = await auth.get(loginFormData.username);
+    if (user === null) {
+      throw new Error("Wrong credentials! Please try again.");
+    }
+
+    const password = user.password as string;
+
+    if (await argon2.verify(password, loginFormData.password)) {
+      const token = jwt.sign({ username: user.key }, jwtSecret, { expiresIn: "21600s" });
+      res.status(200).json({ token, success: true });
+    } else {
+      res.status(401).json({
+        error: "Wrong credentials! Please try again.",
+        success: false
+      });
     }
   } catch (err) {
     res.status(503).json({ error: err.message });
