54020dc9d5de02590446f7a45c32f2b330acabf9
sanqro <contact@sanqro.xyz>
2023-12-20 18:41:33 +0100

    add public to create

--- server/src/routes/board.ts
+++ server/src/routes/board.ts
@@ -2,6 +2,7 @@ import express from "express";
 import * as dotenv from "dotenv";
 import path from "path";
 import { Deta } from "deta";
+import argon2 from "argon2";
 import { ITask, IBoard } from "../interfaces/interfaces";
 dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
@@ -14,9 +15,14 @@ const router = express.Router();
 
 router.post("/create", async (req, res) => {
   try {
+    let passwordHash = "";
     const boardDataSet: IBoard = req.body as IBoard;
     if (typeof boardDataSet.title !== "string" || typeof boardDataSet.owner !== "string") {
       throw new Error("Invalid 'title' or 'owner' in the request.");
+    }
+    
+    if (boardDataSet.public) {
+      passwordHash = await argon2.hash(boardDataSet.password);
     }
 
     const key = boardDataSet.title.trim() + boardDataSet.owner.trim();
@@ -33,7 +39,9 @@ router.post("/create", async (req, res) => {
       key: key,
       title: boardDataSet.title,
       owner: boardDataSet.owner,
-      tasks: tasks
+      tasks: tasks,
+      public: boardDataSet.public,
+      password: passwordHash
     };
 
     const newtaskDataSet = await boardSets.insert(JSON.parse(JSON.stringify(boardDataSetJSON)));
@@ -42,6 +50,23 @@ router.post("/create", async (req, res) => {
       title: boardDataSet.title,
       owner: boardDataSet.owner
     });
+  } catch (err) {
+    res.status(503).json({ error: err.message });
+  }
+});
+
+
+router.get("/getPublic", async (req, res) => {
+  try {
+    const fetchedBoards = await boardSets.fetch({ public: true });
+    if (fetchedBoards == null) {
+      res.status(409).json({
+        error: "This task does not exist."
+      });
+      return false;
+    } else {
+      res.status(201).json({ fetchedBoards });
+    }
   } catch (err) {
     res.status(503).json({ error: err.message });
   }
