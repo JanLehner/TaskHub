a6755a840e0f17d8b6ea5faff6e513475dc0a82c
sanqro <contact@sanqro.xyz>
2023-12-20 21:29:40 +0100

    add endpoint to update task
    Co-authored-by: Niels Brunokowski <77078828+Neox420@users.noreply.github.com>
    Co-authored-by: RelxOff <doeme.kreisel@gmail.com

--- server/src/routes/task.ts
+++ server/src/routes/task.ts
@@ -2,7 +2,7 @@ import express from "express";
 import * as dotenv from "dotenv";
 import path from "path";
 import { Deta } from "deta";
-import { ITask, IBoard } from "../interfaces/interfaces";
+import { ITask, IBoard, ITaskUpdate } from "../interfaces/interfaces";
 dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 // deta setup
@@ -95,4 +95,63 @@ router.post("/delete/:board/", async (req, res) => {
   }
 });
 
+router.post("/update/:board/", async (req, res) => {
+  try {
+    const taskDataSet: ITaskUpdate = req.body as ITaskUpdate;
+    const key = taskDataSet.title.trim() + taskDataSet.owner.trim();
+    const boardTitle = req.params.board;
+    if (!taskDataSet.title || !taskDataSet.owner ) {
+      throw new Error("Both 'title' and 'owner' are required fields.");
+    }
+
+    const taskDataSetJSON = {
+      title: taskDataSet.title,
+      description: taskDataSet.description,
+      dueDate: taskDataSet.dueDate,
+      owner: taskDataSet.owner
+    };
+
+    const updatedTaskDataSetJSON = {
+      title: taskDataSet.newTitle,
+      description: taskDataSet.newDescription,
+      dueDate: taskDataSet.newDueDate,
+      owner: taskDataSet.owner
+    };
+
+    const existing =  await boardSets.get(boardTitle);
+    const currentTasks = existing.tasks;
+
+    // delete old task
+    let taskFound = false;
+    existing.tasks = (currentTasks as any[]).filter(task => {
+      const isMatch = task.title === taskDataSetJSON.title 
+      && task.owner === taskDataSetJSON.owner 
+      && task.description === taskDataSetJSON.description
+      && task.dueDate === taskDataSetJSON.dueDate;
+      if (isMatch) {
+        taskFound = true;
+      }
+      return !isMatch;
+    });
+
+    if (!taskFound) {
+      throw new Error("Task not found.");
+    }
+
+    existing.tasks.push(updatedTaskDataSetJSON);
+
+    await boardSets.delete(boardTitle);
+    const newtaskDataSet = await boardSets.insert(existing);
+
+    res.status(201).json({
+      title: taskDataSet.newTitle,
+      description: taskDataSet.newDescription,
+      dueDate: taskDataSet.newDueDate,
+      owner: taskDataSet.owner,
+      success: true
+    });
+  } catch (err) {
+    res.status(503).json({ error: err.message });
+  }
+});
 export default router;
